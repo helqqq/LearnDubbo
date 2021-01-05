6ea63b9d83071c2c36e54d0fc7891c016683c7d2
helena <helena.lq.he@pensees.ai>
2021-01-05 09:29:26 +0800

    dubbo学习

--- gmall-interface/src/main/java/com/atguigu/gmall/service/OrderService.java
+++ gmall-interface/src/main/java/com/atguigu/gmall/service/OrderService.java
@@ -1,9 +1,5 @@
 package com.atguigu.gmall.service;
 
-import com.atguigu.gmall.bean.UserAddress;
-
-import java.util.List;
-
 public interface OrderService {
-    public List<UserAddress> getUser();
+    public void getUser();
 }
